5b750768a691c982a9bdc649a7d867144d42d0ef
Rajesh Biswas <60763298+RajeshB5@users.noreply.github.com>
2024-01-21 06:45:32 +0530

    Delete SpringBoot ReactJC Curd directory

--- SpringBoot ReactJC Curd/fullStackBootReact/fullStackBootReact/src/main/java/com/curd/fullStackBootReact/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.curd.fullStackBootReact.repository;
-
-import com.curd.fullStackBootReact.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
